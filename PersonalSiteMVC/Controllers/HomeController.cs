1c197756fbed768055e0fc36b413d2d1e8b8a784
Chris Randall <chris.crandall@outlook.com>
2023-09-13 21:00:49 -0500

    Added Contact Functionality

--- PersonalSiteMVC/Controllers/HomeController.cs
+++ PersonalSiteMVC/Controllers/HomeController.cs
@@ -1,18 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PersonalSiteMVC.Models;
 using System.Diagnostics;
+using MimeKit;// Added for access to MimeMessage class
+using MailKit.Net.Smtp;// Added for access to SmtpClient class
 
 namespace PersonalSiteMVC.Controllers
 {
     public class HomeController : Controller
     {
+        //Fields
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IConfiguration _config;
+
+        // Properties
+        // No properties
+
+        // Constructor
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
+        // Methods
         public IActionResult Index()
         {
             return View();
@@ -43,14 +54,64 @@ namespace PersonalSiteMVC.Controllers
             return View();
         }
 
-        public IActionResult Testimonials() 
+        public IActionResult Testimonials()
         {
             return View();
         }
 
-        public IActionResult Contact() 
+        public IActionResult Contact()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            var mm = new MimeMessage();
+
+            string message = $"You have received a new email from your site's contact form!<br/>" +
+                $"Sender: {cvm.Name}<br/>Email: {cvm.Email}<br/>Subject: {cvm.Subject}<br/>" +
+                $"Message: {cvm.Message}";
+
+            mm.From.Add(new MailboxAddress("Sender", _config.GetValue<string>("Credentials:Email:User")));
+            mm.To.Add(new MailboxAddress("Personal", _config.GetValue<string>("Credentials:Email:Recipient")));
+
+            mm.Subject = cvm.Subject;
+            mm.Body = new TextPart("HTML") { Text = message };
+            mm.Priority = MessagePriority.Urgent;
+            mm.ReplyTo.Add(new MailboxAddress("User", cvm.Email));
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect(_config.GetValue<string>("Credentials:Email:Client"));
+
+                client.Authenticate(
+                    _config.GetValue<string>("Credentials:Email:User"),
+                    _config.GetValue<string>("Credentials:Email:Password")
+                    );
+
+                try
+                {
+                    client.Send(mm);
+                }
+                catch (Exception ex)
+                {
+
+                    ViewBag.ErrorMessage = $"There was an error processing your request." +
+                        $"Please try again later.<br/>" +
+                        $"Error Message: {ex.StackTrace}";
+
+                    return View(cvm);  
+                }
+            }
+
+            return View("EmailConfirmation", cvm);
+
         }
     }
 }
---
+++ PersonalSiteMVC/Models/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;// Added to grant access to annotations for validation
+
+namespace PersonalSiteMVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "A message is required")]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
